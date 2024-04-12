39c1c9dd875f865197db6d7641a6b8b97f764307
甘星 <ganxing@fline88.com>
2024-04-12 11:13:03 +0800

    清除node处理图片代码，修改页面结构

--- apps/backend/service.js
+++ apps/backend/service.js
@@ -6,7 +6,6 @@ const cors = require('cors'); // 导入 cors
 const app = express();
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' }); // 设置文件存储路径
-const sharp = require('sharp');
 
 const port = 4000;
 
@@ -20,18 +19,12 @@ app.post('/uploadImage', upload.single('image'), async (req, res) => {
 
   const idStr = String(req.body.id);
   const nameStr = String(req.body.name);
-  const originalDir = path.resolve(__dirname, '../classify/original', idStr);
-  const processedDir = path.resolve(__dirname, '../classify/processed', idStr);
-
-  console.log('Directories:', { originalDir, processedDir });
+  const originalDir = path.resolve(__dirname, '../original', idStr);
 
   // 确保目录存在
   if (!fs.existsSync(originalDir)) {
     console.log('Creating original directory');
     fs.mkdirSync(originalDir, { recursive: true });
-  }
-  if (!fs.existsSync(processedDir)) {
-    fs.mkdirSync(processedDir, { recursive: true });
   }
 
   const originalFilePath = path.join(originalDir, `${nameStr}.png`); // 保存原图
@@ -42,23 +35,7 @@ app.post('/uploadImage', upload.single('image'), async (req, res) => {
       console.error('Error saving the original image:', err);
       return res.status(500).send('Error saving the original image');
     }
-
-    // 随机采样并保存处理后的图像
-    try {
-      for (let i = 1; i <= 16; i++) {
-        // 生成随机裁剪位置
-        const left = Math.floor(Math.random() * (500 - 224));
-        const top = Math.floor(Math.random() * (500 - 224));
-        await sharp(originalFilePath)
-          .extract({ width: 224, height: 224, left: left, top: top })
-          .toFile(path.join(processedDir, `${nameStr}-${i}.png`));
-      }
-
-      res.json({ message: 'Original and processed images saved successfully' });
-    } catch (error) {
-      console.error('Error processing images:', error);
-      res.status(500).send('Error processing images');
-    }
+    res.json({ message: 'Original image saved successfully' });
   });
 });
 
